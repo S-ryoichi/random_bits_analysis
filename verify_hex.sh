7e3d98738b9a972f604fb7d7cfa6b08a89d2f50c
toryo <65220724+S-ryoichi@users.noreply.github.com>
2021-12-22 16:19:55 +0900

    Add files via upload

---
+++ verify_hex.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+mkdir exec
+mkdir bin_ascii_data
+mkdir result
+mkdir log
+
+gcc -o ./exec/arrange_data ./program/arrange_data/file_update.c
+gcc -o ./exec/markov_veryfy ./program/markov_model/The_Markov_model.c
+gcc -o ./exec/prediction2 ./program/nextbit_prediction/2bit_prediction.c
+gcc -o ./exec/prediction3 ./program/nextbit_prediction/3bit_prediction.c
+gcc -g -o ./exec/markov_csv ./program/z-test/markov_1000.c
+# gcc -o ./exec/markov_csv ./program/z-test/markov.c
+
+for filename in ./data/*.txt; do
+    basefile=$(basename $filename .txt)
+    binfilename=./bin_ascii_data/${basefile}_out.txt
+    dirname=./result/${basefile}
+    
+    mkdir $dirname
+    mkdir $dirname/markov_csv
+
+    echo "./exec/arrange_data $filename $binfilename"
+    ./exec/arrange_data $filename $binfilename
+
+    echo "./exec/markov_veryfy $binfilename ${dirname}/${basefile}_markov.txt"
+    ./exec/markov_veryfy $binfilename ${dirname}/${basefile}_markov.txt > ./log/${basefile}_markov.log
+
+    echo "./exec/prediction2 $binfilename ${dirname}/${basefile}_2predict.txt"
+    ./exec/prediction2 $binfilename ${dirname}/${basefile}_2predict.txt > ./log/${basefile}_2predict.log
+
+    echo "./exec/prediction3 $binfilename ${dirname}/${basefile}_3predict.txt"
+    ./exec/prediction3 $binfilename ${dirname}/${basefile}_3predict.txt > ./log/${basefile}_3predict.log
+
+    echo "./exec/markov_csv $binfilename ${dirname}/${basefile}_markov.csv"
+    ./exec/markov_csv $binfilename ${dirname}/markov_csv/${basefile}_markov > ./log/${basefile}_markov.log
+
+    # for csvfilename in ./
+    python3 ./program/z-test/z_test.py ${dirname}/markov_csv/ ${dirname}/${basefile}_z_test.csv > ./log/${basefile}_z_test.log
+done
+
+
+
+###############
+# ls
+# 
+# cp ../log/rand01.txt ./data/rand01.txt
+# 
+# cd program
+# 
+# gcc -o ./out ./file_update.c
+# 
+# ./out
+# 
+# gcc -o ../exec/prediction ./2bit_prediction.c
+# 
+# ../exec/prediction
+# 
+# gcc -o ../exec/prediction ./3bit_prediction.c
+# 
+# ../exec/prediction
+# 
+# gcc -o ../exec/markov  ./The_Markov_model.c
+# 
+# ../exec/markov
+# 
+# gcc -o ../exec/markov_csv ./markov.c
+# 
+# ../exec/markov_csv
+# 
+# python3 csv_analysis.py ../markov_result.csv ../solve_p_value.csv
+# 
+# ./graph.sh
+# 
+# cd ..
+# 
+# mkdir ./result
+# 
+# mv ./*.txt ./*.csv ./*.png ./result
