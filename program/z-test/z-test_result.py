1ce5514dd15e5ec16a5da55689d10f5df7f122a8
toryo <65220724+S-ryoichi@users.noreply.github.com>
2022-01-14 19:21:24 +0900

    Add files via upload

--- program/z-test/z-test_result.py
+++ program/z-test/z-test_result.py
@@ -2,19 +2,19 @@ import os
 import sys
 import pandas as pd
 
-csv_file = os.listdir(".")
+csv_file = os.listdir(sys.argv[1])
 print("f_name is {}".format(csv_file))
-csv_file.remove(".history")
-csv_file.remove("z-test_result.py")
-csv_file.remove("z-test_result.txt")
-csv_file.remove("z-test_result.csv")
+# csv_file.remove(".history")
+# csv_file.remove("z-test_result.py")
+# csv_file.remove("z-test_result.txt")
+# csv_file.remove("z-test_result.csv")
 csv_file_strip = []
 
 def main():
     writecsv = []
     for f_name in csv_file:
         print(f_name)
-        f_data = pd.read_csv("./" + str(f_name), engine="python")
+        f_data = pd.read_csv(sys.argv[1] + str(f_name), engine="python")
         f_booldata = (f_data >= 0.005)
         print(f_booldata.sum())
         writecsv.append(f_booldata.sum())
@@ -24,7 +24,7 @@ def main():
     # fw = fw.rename(columns={"00": "state_00","01": "state_01","10": "state_10","11": "state_11"})
     csv_file_strip = [listname.strip(".csv") for listname in csv_file]
     fw.insert(0, "label", csv_file_strip)
-    fw.to_csv("./z-test_result.csv")
+    fw.to_csv(sys.argv[2])
 
 if __name__ == "__main__":
     main()    
