4dbf8a20d7003b1507a66d1d05e7648635dbea16
toryo <65220724+S-ryoichi@users.noreply.github.com>
2022-02-04 13:23:10 +0900

    Add files via upload

---
+++ program/z-test/graph.py
@@ -0,0 +1,35 @@
+import sys
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def plot_graph(df, name):
+    plt.figure()
+    df.plot(x="label")
+    plt.xlabel("The number of ring oscillator circuits")
+    plt.ylabel("The number of passing test")
+    plt.savefig(sys.argv[2] + name)
+    plt.close("all")
+
+
+def main():
+    csv_data = pd.read_csv(sys.argv[1], engine="python")
+    
+    out = csv_data[["label", "state_00", "state_01", "state_10", "state_11"]]
+    plot_graph(out, "freq.png")
+    
+    out = csv_data[["label", "00->00", "00->01", "00->10", "00->11"]]
+    plot_graph(out, "state_trans_00.png")
+    
+    out = csv_data[["label", "01->00", "01->01", "01->10", "01->11"]]
+    plot_graph(out, "state_trans_01.png")
+    
+    out = csv_data[["label", "10->00", "10->01", "10->10", "10->11"]]
+    plot_graph(out, "state_trans_10.png")
+    
+    out = csv_data[["label", "11->00", "11->01", "11->10", "11->11"]]
+    plot_graph(out, "state_trans_11.png")
+
+
+if __name__ == "__main__":
+    main()
