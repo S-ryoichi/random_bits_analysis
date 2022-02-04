622ce71daabd38d6eb24c2133e8c4dda4b7d0fdf
toryo <65220724+S-ryoichi@users.noreply.github.com>
2022-02-04 13:19:40 +0900

    Add files via upload

--- verify_hex.sh
+++ verify_hex.sh
@@ -36,48 +36,11 @@ for filename in ./data/*.txt; do
     echo "./exec/markov_csv $binfilename ${dirname}/${basefile}_markov.csv"
     ./exec/markov_csv $binfilename ${dirname}/markov_csv/${basefile}_markov > ./log/${basefile}_markov.log
 
-    # for csvfilename in ./
     python3 ./program/z-test/z_test.py ${dirname}/markov_csv/ ./result/z_test/${basefile}.csv
 done
 
 # python3 ./program/z-test/z-test_result.py ./result/z_test/ ./result/z_test_result.csv > ./result/z_test/z-test_result.txt
 python3 ./program/z-test/z-test_result.py ./result/z_test/ ./result/z_test_result.csv
 
-
-
-###############
-# ls
-# 
-# cp ../log/rand01.txt ./data/rand01.txt
-# 
-# cd program
-# 
-# gcc -o ./out ./file_update.c
-# 
-# ./out
-# 
-# gcc -o ../exec/prediction ./2bit_prediction.c
-# 
-# ../exec/prediction
-# 
-# gcc -o ../exec/prediction ./3bit_prediction.c
-# 
-# ../exec/prediction
-# 
-# gcc -o ../exec/markov  ./The_Markov_model.c
-# 
-# ../exec/markov
-# 
-# gcc -o ../exec/markov_csv ./markov.c
-# 
-# ../exec/markov_csv
-# 
-# python3 csv_analysis.py ../markov_result.csv ../solve_p_value.csv
-# 
-# ./graph.sh
-# 
-# cd ..
-# 
-# mkdir ./result
-# 
-# mv ./*.txt ./*.csv ./*.png ./result
+# fot the testing RO circuit
+# python3 ./program/z-test/graph.py ./result/z_test_result.csv
