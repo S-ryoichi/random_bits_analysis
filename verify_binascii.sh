d3d0314e4b3f350333dc2d1e0e44b6dac8e45551
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2022-09-20 01:46:29 +0900

    change directory name

--- verify_binascii.sh
+++ verify_binascii.sh
@@ -7,8 +7,8 @@ mkdir log
 gcc -o ./exec/markov_veryfy ./program/markov_model/The_Markov_model.c
 gcc -o ./exec/prediction2 ./program/nextbit_prediction/2bit_prediction.c
 gcc -o ./exec/prediction3 ./program/nextbit_prediction/3bit_prediction.c
-gcc -g -o ./exec/markov_csv ./program/z-test/markov_1000.c
-# gcc -o ./exec/markov_csv ./program/z-test/markov.c
+gcc -g -o ./exec/markov_csv ./program/t-test/markov_1000.c
+# gcc -o ./exec/markov_csv ./program/t-test/markov.c
 
 for filename in ./bin_ascii_data/*.txt; do
     basefile=$(basename $filename .txt)
@@ -33,15 +33,15 @@ for filename in ./bin_ascii_data/*.txt; do
     ./exec/markov_csv $binfilename ${dirname}/markov_csv/${basefile}_markov > ./log/${basefile}_markov.log
 
     # for csvfilename in ./
-    python3 ./program/z-test/t-test.py ${dirname}/markov_csv/ ./result/t-test/${basefile}.csv
+    python3 ./program/t-test/t-test.py ${dirname}/markov_csv/ ./result/t-test/${basefile}.csv
 done
 
-python3 ./program/z-test/z-test_result.py ./result/t-test/ ./result/t-test_result.csv
+python3 ./program/t-test/t-test_result.py ./result/t-test/ ./result/t-test_result.csv
 
 
 # fot the testing RO circuit
 mkdir ./result/image
-python3 ./program/z-test/graph.py ./result/t-test_result.csv ./result/image/
+python3 ./program/t-test/graph.py ./result/t-test_result.csv ./result/image/
 
 #
-python3 ./program/z-test/chi2test.py ./result/t-test/ ./result/chi2test/
+python3 ./program/t-test/chi2test.py ./result/t-test/ ./result/chi2test/
