f9c36effbac10e4d643fcc2bc41c1bc451af1fec
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2022-09-20 02:34:27 +0900

    same the t-test_result.py

--- program/t-test/t-test_result.py
+++ program/t-test/t-test_result.py
@@ -23,7 +23,7 @@ def main():
     fw = pd.DataFrame(writecsv, columns=["state_00", "state_01", "state_10", "state_11", "00->00", "00->01", "00->10", "00->11", "01->00", "01->01", "01->10", "01->11", "10->00", "10->01", "10->10", "10->11", "11->00", "11->01", "11->10", "11->11"])
     # fw = pd.DataFrame(writecsv, columns=["00", "01", "10", "11", "00->00", "00->01", "00->10", "00->11", "01->00", "01->01", "01->10", "01->11", "10->00", "10->01", "10->10", "10->11", "11->00", "11->01", "11->10", "11->11"])
     # fw = fw.rename(columns={"00": "state_00","01": "state_01","10": "state_10","11": "state_11"})
-    csv_file_strip = [listname.strip(".csv") for listname in csv_file]
+    csv_file_strip = [listname.replace(".csv", "") for listname in csv_file]
     fw.insert(0, "label", csv_file_strip)
     fw.to_csv(sys.argv[2])
 
