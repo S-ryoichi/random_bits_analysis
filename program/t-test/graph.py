4ab3928cecc3a8c3a436b8e5dffa5cb48f926809
S-ryoichi <phdh1ooa@s.okayama-u.ac.jp>
2023-07-30 15:27:41 +0900

    update the program that the output figure changes from line graph to bar graph

--- program/t-test/graph.py
+++ program/t-test/graph.py
@@ -4,6 +4,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def plot_graph(df, name):
+    print(df)
+    print(name)
     plt.figure()
     df.plot(x="label")
     plt.xlabel("The number of ring oscillator circuits")
@@ -14,24 +16,32 @@ def plot_graph(df, name):
     plt.savefig(sys.argv[2] + name)
     plt.close("all")
 
+def plot_bar_graph(df, name):
+    plt.figure()
+    df.plot.bar(ylim=[0,1000])
+    plt.xlabel("The number of ring oscillator circuits")
+    plt.ylabel("The number of passing test")
+
+    plt.savefig(sys.argv[2] + name)
+    plt.close("all")
 
 def main():
     csv_data = pd.read_csv(sys.argv[1], engine="python")
     
     out = csv_data[["label", "P(00)", "P(01)", "P(10)", "P(11)"]]
-    plot_graph(out, "freq.png")
+    plot_bar_graph(out, "freq.png")
     
     out = csv_data[["label", "P(00|00)", "P(01|00)", "P(10|00)", "P(11|00)"]]
-    plot_graph(out, "state_trans_00.png")
+    plot_bar_graph(out, "state_trans_00.png")
     
     out = csv_data[["label", "P(00|01)", "P(01|01)", "P(10|01)", "P(11|01)"]]
-    plot_graph(out, "state_trans_01.png")
+    plot_bar_graph(out, "state_trans_01.png")
     
     out = csv_data[["label", "P(00|10)", "P(01|10)", "P(10|10)", "P(11|10)"]]
-    plot_graph(out, "state_trans_10.png")
+    plot_bar_graph(out, "state_trans_10.png")
     
     out = csv_data[["label", "P(00|11)", "P(01|11)", "P(10|11)", "P(11|11)"]]
-    plot_graph(out, "state_trans_11.png")
+    plot_bar_graph(out, "state_trans_11.png")
 
 
 if __name__ == "__main__":
